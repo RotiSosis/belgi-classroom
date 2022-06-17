5800cd6a270c97ec22a6ce5b8447e0cbb8ee5496
RotiSosis <raflygaret229@gmail.com>
2022-06-18 00:02:07 +0800

    attempt4

---
+++ AttemptFour/app/src/main/java/com/example/miniproject1/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.miniproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements rv_interface{
+    RecyclerView recyclerView;
+    LinearLayoutManager linearLayoutManager;
+    AdapterData adapterData;
+    List<String> listData;
+    List<Integer> pic;
+
+    int [] image = {
+            R.drawable.flat_01,
+            R.drawable.flat_02,
+            R.drawable.flat_03,
+            R.drawable.flat_04,
+            R.drawable.flat_05,
+            R.drawable.flat_06,
+            R.drawable.flat_07,
+            R.drawable.flat_08,
+            R.drawable.flat_09,
+            R.drawable.flat_01,
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyvlerViewData);
+        listData = new ArrayList<>();
+        pic = new ArrayList<>();
+
+        for(int p=0; p<10; p++){
+            pic.add(image[p]);
+        }
+        
+        listData = Arrays.asList(getResources().getStringArray(R.array.itemKelas));
+
+
+        linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapterData = new AdapterData(this, listData, pic, this);
+        recyclerView.setAdapter(adapterData);
+        adapterData.notifyDataSetChanged();
+
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        Intent intent = new Intent(MainActivity.this, activity_item.class);
+
+        intent.putExtra("image_s", pic.get(position));
+        intent.putExtra("kelas_s", listData.get(position));
+
+        startActivity(intent);
+    }
+
+}
